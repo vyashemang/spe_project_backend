90ba555e90b7ef2d3c448031023aba207904522f
Hemang Vyas <vyashemang563@gmail.com>
2021-05-15 11:31:01 +0530

    added logs for elk

--- src/main/java/com/example/spe_project/controller/AnnouncementRestAPIs.java
+++ src/main/java/com/example/spe_project/controller/AnnouncementRestAPIs.java
@@ -2,6 +2,9 @@ package com.example.spe_project.controller;
 
 import com.example.spe_project.model.Announcement;
 import com.example.spe_project.service.AnnouncementService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +18,8 @@ public class AnnouncementRestAPIs {
 
     private final AnnouncementService announcementService;
 
+    private static final Logger logger = LoggerFactory.getLogger(AnnouncementRestAPIs.class);
+
     @Autowired
     public AnnouncementRestAPIs(AnnouncementService announcementService) {
         this.announcementService = announcementService;
@@ -22,30 +27,50 @@ public class AnnouncementRestAPIs {
 
     @GetMapping("/all")
     public List<Announcement> getAnnouncement(){
+        logger.info("get all announcement");
         return announcementService.getAnnouncements();
     }
 
     @GetMapping(path = "/get/{id}")
     public Announcement getAnnouncement(@PathVariable("id") Long id){
-
-        Announcement announcement = announcementService.getAnnouncement(id);
+        Announcement announcement = null;
+        try {
+            announcement = announcementService.getAnnouncement(id);
+            logger.info("get " + id + " announcement");
+        }
+        catch (IllegalStateException e){
+            logger.error("announcement " + id + " not found");
+        }
         return announcement;
     }
 
     @PostMapping("/add")
     public void addNewAnnouncement(@RequestBody Announcement announcement){
         announcementService.addAnnouncement(announcement);
+        logger.info("add announcement");
     }
 
     @DeleteMapping(path = "/delete/{id}")
     public void deleteAnnouncement(@PathVariable("id") Long id){
-        announcementService.deleteAnnouncement(id);
+        try{
+            announcementService.deleteAnnouncement(id);
+            logger.info("delete " + id + " announcement");
+        }
+        catch(IllegalStateException e) {
+            logger.error("announcement " + id + " not found");
+        }
     }
 
     @GetMapping(path = "/update/{id}")
     public void updateAnnouncement(@PathVariable("id") Long id,
                               @RequestParam(required = false) String title,
                               @RequestParam(required = false) String description){
-        announcementService.updateAnnouncement(id, title, description);
+        try{
+            announcementService.updateAnnouncement(id, title, description);
+            logger.info("update" + id +" announcement");
+        }
+        catch (IllegalStateException e) {
+            logger.error("announcement " + id + " not found");
+        }
     }
 }
